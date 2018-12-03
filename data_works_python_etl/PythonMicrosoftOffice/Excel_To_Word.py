e54f05d712ff62fe1e35970cb0f6ce80219b7a5b
guanqifeng <guanqifeng@koudailc.com>
2018-12-03 18:04:01 +0800

    20181203 update OfficeTrans

--- data_works_python_etl/PythonMicrosoftOffice/Excel_To_Word.py
+++ data_works_python_etl/PythonMicrosoftOffice/Excel_To_Word.py
@@ -1,6 +1,7 @@
 import docx
 import sys
 import xlrd
+from progressbar import *
 
 def read_excel(excel_path='file.xls', sheet=0):
     try:
@@ -44,13 +45,25 @@ def update_Paragraph_of_docx(excel_row, doc_obj):
     run.font.name = '宋体'
     run.font.size = 155000
 
-if len(sys.argv) < 3 or len(sys.argv) > 3:
-    print(len(sys.argv))
-    print(str(sys.argv))
-    print("传入参数有问题，需要两条参数（Excel文件路径 与 Word文件路径）！")
-    sys.exit()
-else:
-    excel_path, doc_path = sys.argv[1], sys.argv[2]
+# if len(sys.argv) < 3 or len(sys.argv) > 3:
+#     print(len(sys.argv))
+#     print(str(sys.argv))
+#     print("传入参数有问题，需要两条参数（Excel文件路径 与 Word文件路径）！")
+#     sys.exit()
+# else:
+#     excel_path, doc_path = sys.argv[1], sys.argv[2]
+#     excel_table_obj = read_excel(excel_path, 0)  # 读取Excel文件,返回Excel对象
+#     getExcelList = traversal_of_excel(excel_table_obj)
+#     progress = ProgressBar()
+#     for excelRow in progress(getExcelList):
+#         try:
+#             docObj = read_docx(doc_path)  # 读取Word文件,返回Word对象
+#             update_Paragraph_of_docx(excelRow, docObj)
+#             update_table_of_docx(excelRow, docObj, doc_path)
+#         except Exception as e:
+#             print("程序处理有误：" + str(e))
+
+def main(excel_path,doc_path):
     excel_table_obj = read_excel(excel_path, 0)  # 读取Excel文件,返回Excel对象
     getExcelList = traversal_of_excel(excel_table_obj)
     for excelRow in getExcelList:
@@ -61,18 +74,7 @@ else:
         except Exception as e:
             print("程序处理有误：" + str(e))
 
-# def main(excel_path,doc_path):
-#     excel_table_obj = read_excel(excel_path, 0)  # 读取Excel文件,返回Excel对象
-#     getExcelList = traversal_of_excel(excel_table_obj)
-#     for excelRow in getExcelList:
-#         try:
-#             docObj = read_docx(doc_path)  # 读取Word文件,返回Word对象
-#             update_Paragraph_of_docx(excelRow, docObj)
-#             update_table_of_docx(excelRow, docObj, doc_path)
-#         except Exception as e:
-#             print("程序处理有误：" + str(e))
-#
-# if __name__ == '__main__':
-#     excel_path = 'C:\\Users\\Administrator\\Desktop\\trans\\甲方信息表(1)(1).xlsx'
-#     doc_path = 'C:\\Users\\Administrator\\Desktop\\trans\\通联支付网络服务股份有限公司当面付业务协议-模板(2).docx'
-#     main(excel_path,doc_path)
+if __name__ == '__main__':
+    excel_path = 'C:\\Users\\Administrator\\Desktop\\trans\\甲方信息表(1)(1).xlsx'
+    doc_path = 'C:\\Users\\Administrator\\Desktop\\trans\\通联支付网络服务股份有限公司当面付业务协议-模板(2).docx'
+    main(excel_path,doc_path)
